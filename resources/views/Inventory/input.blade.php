569031f08ea417aa752c69c86c0406c7a24a932c
Farida964 <imfaravi@gmail.com>
2025-12-26 13:29:54 +0700

    revisi 2

--- resources/views/Inventory/input.blade.php
+++ resources/views/Inventory/input.blade.php
@@ -1,21 +1,75 @@
-<form action="{{ route('inventory.storeInput', $inventory->id) }}" method="POST">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+     <link rel="stylesheet" href="{{ asset('assets/css/createinv.css') }}">
+</head>
+<body>
+<section class="form-wrapper">
+    <form action="{{ route('inventory.storeInput', $inventory->id) }}" method="POST" class="form-box">
     @csrf
 
     <!-- FIELD TIDAK BISA DIUBAH -->
-    <input type="text" value="{{ $inventory->kode }}" readonly>
-    <input type="text" value="{{ $inventory->nama }}" readonly>
-    <input type="text" value="{{ $inventory->warna }}" readonly>
-    <input type="text" value="{{ $inventory->ukuran }}" readonly>
-    <input type="text" value="{{ $inventory->harga }}" readonly>
-    <input type="text" value="{{ $inventory->keuntungan }}" readonly>
-    <input type="text" value="{{ $inventory->keterangan }}" readonly>
+     <div class="row">
+        <label>KODE PRODUK :</label>
+       <input type="text" value="{{ $inventory->kode }}" readonly>
+    </div>
+
+    <div class="row">
+        <label>NAMA PRODUK :</label>
+           <input type="text" value="{{ $inventory->nama }}" readonly>
+    </div>
+
+    <div class="row">
+        <label>WARNA :</label>
+            <input type="text" value="{{ $inventory->warna }}" readonly>
+    </div>
+
+    <div class="row">
+        <label>UKURAN :</label>
+            <input type="text" value="{{ $inventory->ukuran }}" readonly>
+    </div>
+
+    <div class="row">
+        <label>HARGA :</label>
+            <input type="text" value="{{ $inventory->harga }}" readonly>
+    </div>
+
+    <div class="row">
+        <label>KEUNTUNGAN :</label>
+            <input type="text" value="{{ $inventory->keuntungan }}" readonly>
+    </div>
+
+     <div class="row">
+        <label>KETERANGAN :</label>
+        <input type="text" value="{{ $inventory->keterangan }}" readonly>
+    </div>
+
+    
 
     <!-- FIELD INPUT -->
-    <label>Masuk</label>
-    <input type="number" name="masuk" value="0" min="0">
 
-    <label>Keluar</label>
-    <input type="number" name="keluar" value="0" min="0">
+    <div class="row">
+        <label>MASUK :</label>
+        <input type="number" name="masuk" value="0" min="0">
+    </div>
 
-    <button type="submit">Simpan</button>
-</form>
+     <div class="row">
+        <label>KELUAR :</label>
+        <input type="number" name="keluar" value="0" min="0">
+    </div>
+
+
+
+     <!-- BUTTONS -->
+            <div class="btn-group">
+                <a href="{{ route('inventory.index') }}" class="btn cancel">Batal</a>
+                <button type="submit" class="btn add">Simpan</button>
+            </div>
+    </form>
+
+    </section>
+</body>
+</html>
